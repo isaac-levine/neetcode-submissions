0d906386f744034d7a0569955ef6bb883565a2f2
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-05-12 19:17:55 -0400

    Add: redundant-connection - submission-3

---
+++ Data Structures & Algorithms/redundant-connection/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        
+        # iterate through the edges
+            # if a, b are already connected (same parent), then add this edge to the list of duplicate edges. 
+            # union the two nodes. 
+
+        # return the last edge in the list of duplicate edges. 
+
+        N = len(edges)
+        par = [i for i in range(N + 1)]
+        def union(a, b):
+            par[find(b)] = find(a)
+
+        def find(a):
+            if par[a] != a:
+                return find(par[a])
+            else:
+                return a
+
+        badEdges = [] 
+        for a, b in edges:
+            if find(a) == find(b):
+                badEdges.append([a, b])
+            union(a, b)
+        
+        return badEdges[len(badEdges) - 1]
