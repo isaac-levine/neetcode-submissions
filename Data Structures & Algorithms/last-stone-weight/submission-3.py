91c781d14817a6dce690650831db66b7e073dead
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-05-13 08:34:43 -0400

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        # follow the simulation until there is one stone remaining and return its weight 
+        heap = [-w for w in stones]
+        heapq.heapify(heap)
+        while len(heap) > 1:
+            x, y = heapq.heappop(heap), heapq.heappop(heap)
+            if x == y:
+                continue
+            else:
+                diff = abs(x - y)
+                heapq.heappush(heap, diff * -1)
+
+        return heap[0] * -1 if heap else 0
