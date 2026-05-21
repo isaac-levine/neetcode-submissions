ca96c243309b4e30548d857e134340ce5b2979df
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-05-21 19:05:10 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+
+        # return number of distinct ways to climb to stair n
+        # you can climb 1 or 2 steps at a time 
+
+        
+        # 1 --> 1 --> 1
+        # 2 --> 1,2  | 2 --> 2
+        # 3 --> 1, 2, 3 | 1, 3 | 2, 3 --> 3
+        # 4 --> 1,2,3,4 | 1,3,4 | 2,3,4 | 2,4 | 1,2,4 --> 5
+
+
+        # intuition: there's x ways to get to n - 2 and y ways to get to n - 1.  but from n - 2 to n there's only 1 way without touching n -1, and from n - 1 to n there's only 1 way
+        # so there's always memo[n - 2] + memo[n - 1] ways to get to n 
+
+
+        one, two = 1, 1
+
+        for i in range(n):
+            tmp = one
+            one = one + two # one behind becomes "cur"
+            two = tmp # two behind becomes what was previously one behind
+
+        return two
