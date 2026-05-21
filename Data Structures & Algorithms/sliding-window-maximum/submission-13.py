2a8656e46ec04a0ed2640f27a9594c2dc57ce185
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-05-21 18:32:22 -0400

    Add: sliding-window-maximum - submission-13

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-13.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+
+
+        q = deque() # monotonically decreasing --> 
+                    # top/right of the q is the smallest 
+                    # back/left of the q is the biggest.
+        res = []
+        l = 0
+
+        for r in range(len(nums)):
+
+            while q and nums[q[-1]] < nums[r]:
+                q.pop() # pop from the top of the queue until top of the queue is smaller than nums[r]
+
+            q.append(r)
+
+            # check if we need to pop out the left 
+            if q[0] < l:
+                q.popleft() 
+
+            # only add to res once window is right size
+            if r >= k - 1:
+                res.append(nums[q[0]])
+                l += 1
+
+        return res
