0bb0b238028b469da0d8ba26d5719c28d15a6b36
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-06-21 08:54:55 -0400

    Add: design-word-search-data-structure - submission-7

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-7.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self, c):
+        self.val = c
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode(None)
+        
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word: 
+            if c not in cur.children:
+                cur.children[c] = TrieNode(c)
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(i, cur):
+            if i == len(word):
+                return cur.endOfWord # we've matched all characters but need to make sure we're at the end of the word 
+            elif word[i] == ".":
+                for c in cur.children: # . means we need to explore all possible next characters/nodes
+                    if dfs(i + 1, cur.children[c]):
+                        return True
+                # return False
+            elif word[i] in cur.children:
+                return dfs(i + 1, cur.children[word[i]]) # explore explicitly this next node -- normal search
+            return False # if we fell through to here, that means we never returned true for this search call
+
+
+        return dfs(0, self.root)
