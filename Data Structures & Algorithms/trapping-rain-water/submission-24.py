a604c96252e6e992defa32480a8b4bda40ae96a4
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-06-25 20:14:34 -0400

    Add: trapping-rain-water - submission-24

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-24.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l]) # we know rightMax is bigger, so this is the binding constraint 
+                res += leftMax - height[l] # if leftMax JUST got updated above, then this would be 0. cant be negative b/c of above
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
