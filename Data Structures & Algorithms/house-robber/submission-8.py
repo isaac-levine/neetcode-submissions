2fb3a0119d6e109c77a4933a7525213575751669
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-07-08 20:45:55 -0400

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        profit = [0] * len(nums)
+        profit[0], profit[1] = nums[0], max(nums[1], nums[0])
+
+        for i in range(2, len(nums)):
+            profit[i] = max(profit[i - 2] + nums[i], profit[i - 1]) # can take close or far + nums[i]
+        
+        return profit[len(nums) - 1]
+
+            
+        
