05d76ea87238e38589ef3c27c20dee217b748de0
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-07-16 19:26:44 -0400

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        N = len(prices)
+        #    [[buying, selling]]
+        dp = [[0, 0] for _ in range(N + 2)] # dp[i][0] -> max profit if we are in buying state
+                                            # dp[i][1] -> max profit if we are in selling state
+        for i in range(N - 1, -1, -1):
+            
+            # if we are in "buying" state today. deciding to buy or hold 
+            dp[i][0] = max(
+                dp[i + 1][1] - prices[i], # buy: must be in selling state tomorrow
+                dp[i + 1][0] # hold: maybe buy tomorrow  
+            )
+
+            # if we are in "selling" state today. deciding to sell or hold 
+            dp[i][1] = max(
+                dp[i + 2][0] + prices[i], # sell: must skip one day and then enter buying state
+                dp[i + 1][1] # hold: maybe sell tomorrow
+            )
+
+        return dp[0][0]
