0376f2aea37bb29abcee9eb36b3d9549423b8e21
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-07-24 18:25:10 -0400

    Add: merge-intervals - submission-6

---
+++ Data Structures & Algorithms/merge-intervals/submission-6.py
@@ -0,0 +1,36 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+
+        # merge the intervals by star ttime 
+        # build res
+        # always check for overlap before adding, othewrwise overwrite res[-1]
+
+        # O(n log n)
+        intervals.sort() # how do you sort based on start time? 
+
+
+        # O(n) to build res 
+
+        res = [] 
+
+        for start, end in intervals:
+            
+            # res is empty
+            if not res:
+                res.append([start, end])
+            
+            # tail overlaps with [start, end]
+            if start <= res[-1][1]:
+                res[-1][0] = min(res[-1][0], start)
+                res[-1][1] = max(res[-1][1], end)
+            else: # tail does not overlap
+                res.append([start, end])
+
+        
+        return res
+            
+            
+
+
+            
