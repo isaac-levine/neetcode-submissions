0496f1192c5c33ffe7cff33ef7d31fc34c975320
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-07-30 17:54:50 -0400

    Add: valid-parenthesis-string - submission-5

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-5.py
@@ -0,0 +1,42 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+
+        # "((**)"
+
+        # s="()"
+        
+        openers = []
+        stars = []
+        
+
+        for i in range(len(s)):
+            c = s[i]
+            if c == "(":
+                print("found an opener")
+                openers.append(i)
+            elif c == "*":
+                stars.append(i)
+            elif c == ")":
+                print("found a closer")
+                if openers: # case 1: you have an opener to close.
+                    print("--- popped an opener")
+                    openers.pop()
+                elif not openers and stars: # case 2: you dont have an opener to close but you have a star
+                    print("--- popped a star")
+                    stars.pop()
+                elif not openers and not stars: # case 3: you dont have either. failed. 
+                    return False
+
+        # process what's leftover. 
+        while openers:
+            if not stars:
+                return False
+            openerIndex = openers.pop() # this is the furthest right openerIndex. 
+            # is there a star index to it's right?
+            starIndex = stars.pop()
+            if openerIndex > starIndex:
+                return False
+
+        return True
+
+                
