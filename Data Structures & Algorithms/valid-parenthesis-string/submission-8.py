76526ce08dde7e6720d1d06478f63952f5129012
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-07-30 17:56:39 -0400

    Add: valid-parenthesis-string - submission-8

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-8.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openers = []
+        stars = []
+        
+        for i, c in enumerate(s):
+            c = s[i]
+            if c == "(":
+                openers.append(i)
+            elif c == "*":
+                stars.append(i)
+            elif c == ")":
+                if openers: # case 1: you have an opener to close.
+                    openers.pop()
+                elif not openers and stars: # case 2: you dont have an opener to close but you have a star
+                    stars.pop()
+                elif not openers and not stars: # case 3: you dont have either. failed. 
+                    return False
+
+        # process what's leftover. 
+        while openers and stars:
+            if openers.pop() > stars.pop():
+                return False
+
+        return not openers # there can be stars leftover.
+
+                
