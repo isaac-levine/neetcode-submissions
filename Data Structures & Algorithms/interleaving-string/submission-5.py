b70ceadfec0ffa0b0d16080a740062aa3937c797
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-01 23:14:35 -0400

    Add: interleaving-string - submission-5

---
+++ Data Structures & Algorithms/interleaving-string/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+
+        if len(s1) + len(s2) != len(s3): return False
+        
+
+        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        dp[len(s1)][len(s2)] = True
+
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                # if this letter s3[i+j] can be formed using s1 or s2
+                if (
+                        i < len(s1) and s3[i + j] == s1[i] and dp[i + 1][j] or 
+                        j < len(s2) and s3[i + j] == s2[j] and dp[i][j + 1] 
+                   ):
+                    dp[i][j] = True
+
+
+        return dp[0][0]
