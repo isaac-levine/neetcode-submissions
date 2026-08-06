c5f2d6c48bb26d1a187a383a3caa191b247540f4
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-05 21:29:57 -0400

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        def cost(i: List[int], j: List[int]) -> int:
+            return abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+
+        # return minimum total cost to connect all points together
+        
+        # the trick of this problem is the graph is complete: there's no setup to do
+        # nodes = 0...n-1 in points[] 
+        
+
+        minHeap = [(0, 0)] # just (edge_weight, index) we don't care what the source was
+        visited = set()
+        totalCost = 0
+
+        while minHeap:
+
+            node_cost, node_index = heapq.heappop(minHeap)
+            if node_index in visited:
+                continue 
+            visited.add(node_index)
+            totalCost += node_cost 
+            
+
+
+            for i in range(len(points)):
+                if i in visited:
+                    continue
+                heapq.heappush(minHeap, (cost(node_index, i), i))
+        
+        return totalCost 
