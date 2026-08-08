c723c6671f9a7bc7d9b99bde5c5270ce41bb491e
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-08 07:08:29 -0400

    Add: network-delay-time - submission-9

---
+++ Data Structures & Algorithms/network-delay-time/submission-9.py
@@ -0,0 +1,47 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        
+        # minimum time it takes for all of the nodes to receive the signal 
+
+        # k is the source node 
+
+        # dijkstra's from source node k
+        # then just validate that we've visited every node len(visted) == n 
+        # result will be number of iterations / layers of the BFS.
+
+
+        # build an adjacency list
+        adj = {i: set() for i in range(1, n + 1)}
+        for s, d, t in times:
+            adj[s].add((t, d)) # s : (edgeCost, dest)
+
+        # 1: (1,2), (4,4)
+        # 2: (1,3)
+        # 3: (1,4)
+        # 4: 
+
+        # minHeap = [ (1,2), (4,1) ]
+
+        # res = 1
+        visited = set()
+        minHeap = [(0, k)] # (totalCost, dest)
+        res = 0
+        # visited = set() -- do we need a visited set for dijkstra's?
+
+        while minHeap:
+            # The heap always surrenders the global minimum of the frontier, so (4,4) can only pop once everything cheaper is gone 
+            time, node = heapq.heappop(minHeap)
+            if node in visited:
+                continue
+
+            # add the frontier 
+            for neiTime, neiNode in adj[node]:
+                # if neiNode not in visited:
+                heapq.heappush(minHeap, (time + neiTime, neiNode)) # push (totalTime, neighborNode) to heap
+
+
+            res = time
+            visited.add(node)
+
+
+        return res if len(visited) == n else -1 
