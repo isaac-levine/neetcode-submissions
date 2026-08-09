ae90bd0dcf17073c8e9c344bfe6f64813895bff3
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-09 18:27:03 -0400

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # process one layer at a time BFS style. and the amount of layers/levels is our answer
+
+        # [2, 4,1,1,1,1]
+
+        # 
+        # jumps = 0
+        
+        l = r = 0
+        jumps = 0
+        while r < len(nums) - 1:
+            jumps += 1
+            newL = r + 1
+            for i in range(l, r + 1):
+                r = max(r, i + nums[i])
+            l = newL
+
+        return jumps
+
