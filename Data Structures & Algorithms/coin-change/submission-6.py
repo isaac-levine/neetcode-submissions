60a393ccb40ef3c8944328bfc8ab6704aa3a27b7
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-10 06:41:03 -0400

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+
+        dp = [float("inf")] * (amount + 1) # dp[i] = fewest number of coins needed to make target amount i.
+        dp[0] = 0
+
+        for t in range(1, amount + 1):
+            # can we make target t with the coins? 
+            for c in coins:
+                if t >= c and dp[t - c] != float("inf"):
+                    dp[t] = min(dp[t - c] + 1, dp[t])
+
+        return dp[amount] if dp[amount] != float("inf") else -1 
