8118180d0aa2255e97eaa71cae0e8052c10b0973
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-21 20:14:10 -0400

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # permutations -> backtracking (n-ary?)
+
+        
+        res = [] 
+        n = len(nums)
+
+        # decision tree: which num to pick next for the result we're building 
+        
+
+        #                       [1,2,3]
+        #                       [_,_,_] i = 0 -- where can we put 1
+        #               [1,_,_] [_,1,_] [_,_,1] i = 1 -- where can we put 2
+        # [1,2,_] [1,_,2]   [2,1,_] [_,1,2]    [2,_,1] [_,2,1] -- where can we put 3?
+
+        def backtrack(i, cur):
+            if i == n:
+                res.append(cur)
+                return
+            
+            for j in range(n):
+                if cur[j] == float("inf"):
+                    backtrack(i + 1, cur[:j] + [nums[i]] + cur[j + 1:])
+
+        backtrack(0, ([float("inf")] * n))
+        return res
