3e901c5a08dad7c3b2e2817c8ab4029fc99bbca9
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-22 07:03:00 -0400

    Add: swim-in-rising-water - submission-4

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-4.py
@@ -0,0 +1,38 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        
+        # going from src (0,0) to dest (n - 1, n - 1) --> bfs/dijkstra's/bellman-ford family 
+
+        # optimize for max height seen so far.
+        # and for each layer of BFS just increase time 
+
+        # so use dijkstra's for shortest path, but instead of cost use maxHeightSeenSofar.
+        # there is no egde cost between things, just time and worst case height
+
+        
+        minHeap = [(grid[0][0], 0, 0)] # (maxSeenHeight, (r, c))
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        n = len(grid)
+        visited = set()
+        res = grid[0][0]
+
+        while minHeap:
+            maxHeight, r, c = heapq.heappop(minHeap)
+            if (r, c) in visited:
+                continue
+            if r == n - 1 and c == n - 1:
+                return maxHeight
+
+            visited.add((r, c))
+            res = max(res, maxHeight)
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if nr < 0 or nc < 0 or nr >= n or nc >= n or (nr, nc) in visited:
+                    continue
+                heapq.heappush(minHeap, (max(maxHeight, grid[nr][nc]), nr, nc))
+
+
+        return res
+
+         
