863b58c8bf9ecd0a5da72efe9671bc3c0247aa10
Isaac Levine <80065258+isaac-levine@users.noreply.github.com>
2026-08-24 05:54:19 -0400

    Add: reconstruct-flight-path - submission-5

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-5.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+
+        # MST algorithms
+        # prim's
+        # the union find one... Bellman-Ford? 
+
+        # wrong. it's not an MST problem. You don't need to visit every node. You just need to get from src to dest....
+
+        # eulerian path....wtf even is that
+
+        # hierholzer's algorithm for finding an eulerian path in a graph...
+        # walk until stuck, append, unwind (implicit return in this case)
+
+        # we know there is at least one valid path.
+
+        tickets.sort()
+        adj = defaultdict(deque) 
+        for src, dest in tickets:
+            adj[src].append(dest)
+        
+        res = [] # builds in reverse order because we walk all the way to the end first.
+
+        def dfs(node):
+
+
+            # we are not using a node visited set, instead we are just popping off 'edges' essentially because we could end up 
+            # revisiting a node in a valid solution
+
+            while adj[node]:
+                # why are we popping and modifying the neighbors instead of jsut looping through?
+                # is this to kind of avoid having to use a visited set? 
+                neighbor = adj[node].popleft()
+                dfs(neighbor)
+
+            res.append(node)
+
+        dfs("JFK")
+        return res[::-1]
